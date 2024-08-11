d3553039729dc899e324cabdef3de8f844591791
qufeng107 <qufeng107@gmail.com>
2024-08-11 21:09:34 +0100

    Reverse Linked List - Go - traverse

--- Linked List/Reverse Linked List/solutions.go
+++ Linked List/Reverse Linked List/solutions.go
@@ -2,7 +2,7 @@
  * @Author: qufeng107 qufeng107@gmail.com
  * @Date: 2024-08-11 17:05:35
  * @LastEditors: qufeng107 qufeng107@gmail.com
- * @LastEditTime: 2024-08-11 17:05:56
+ * @LastEditTime: 2024-08-11 21:07:51
  * @FilePath: /Algorithms/Linked List/Reverse Linked List/solutions.go
  * @Description:
  *
@@ -17,12 +17,33 @@
  * }
  */
 
-func reverseBookList(head *ListNode) []int {
+// recursion
+func reverseBookList1(head *ListNode) []int {
 
 	if head == nil {
 		return []int{}
 	}
 
 	return append(reverseBookList(head.Next), head.Val)
+
+}
+
+// traverse linked list to get length, traverse linked list again to get value
+func reverseBookList2(head *ListNode) []int {
+
+	length := 0
+	tempHead := head
+	for tempHead != nil {
+		length++
+		tempHead = tempHead.Next
+	}
+
+	reverse_list := make([]int, length)
+	for i := 1; head != nil; i++ {
+		reverse_list[length-i] = head.Val
+		head = head.Next
+	}
+
+	return reverse_list
 
 }
