cbb022eb9f57610ad85d348aade7f139844d7641
qufeng107 <qufeng107@gmail.com>
2024-08-14 21:58:09 +0100

    Remove Linked List Node - Python - update

--- Linked List/Remove Linked List Node/solution.py
+++ Linked List/Remove Linked List Node/solution.py
@@ -17,7 +17,7 @@ class ListNode:
 
 
 
-class Solution:
+class Solution1:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
 
         # remove head
@@ -27,7 +27,6 @@ class Solution:
         # check head
         if head is None:
             return None
-
 
         pointer = head
 
